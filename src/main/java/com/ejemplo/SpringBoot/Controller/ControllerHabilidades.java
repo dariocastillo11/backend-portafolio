2ca3916a55def5363445ae8efc746b2596b6ef2b
dariocastillo11 <dario.4979102@gmail.com>
2023-02-24 21:00:08 -0300

    estilo de fondo

--- src/main/java/com/ejemplo/SpringBoot/Controller/ControllerHabilidades.java
+++ src/main/java/com/ejemplo/SpringBoot/Controller/ControllerHabilidades.java
@@ -15,9 +15,9 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/habilidades")
-@CrossOrigin(origins = "https://portafolio-dariocastillo11.web.app")
-//@CrossOrigin(origins = "http://localhost:4200")
-public class ControllerHabilidades {
+//@CrossOrigin(origins = "https://portafolio-dariocastillo11.web.app")
+@CrossOrigin(origins = "http://localhost:4200")
+public class ControllerHabilidsades {
 
     @Autowired
     private HabilidadesService haServ;
